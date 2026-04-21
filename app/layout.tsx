cf10f87349013d5ba03b2b8b4dfd9e095e5a6253
yoepo2026-alt <yoepo2026@gmail.com>
2026-04-21 17:46:06 -0300

    iconos

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,12 @@ export default function RootLayout({
       suppressHydrationWarning
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head> 
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest"></link>
+      </head>
       <body className="min-h-full flex flex-col bg-white dark:bg-slate-900 transition-colors">
         <Providers>{children}</Providers>
       </body>
